820d1b3c1502b79cc96d20e8636393f0f7ca1728
myhzy <notmyhzy@hotmail.com>
2025-08-25 09:18:52 +0100

    added approved container for approved holidays

--- src/app/features/NotificationPanel.tsx
+++ src/app/features/NotificationPanel.tsx
@@ -7,10 +7,17 @@ export default function NotificationPanel() {
       <h2>Today</h2>
 
       <div className="notification-alerts-container">
-        <p>Your holiday request has been approved!</p>
-        <p>new noti</p>
-        <p>new noti</p>
-        <p>new noti</p>
+        <div className="holiday-decision-container">
+          <div className="approved-status">
+            <p>✅</p>
+            <h1>Approved!</h1>
+          </div>
+
+          <div className="approved-status-type">
+            <h2>17-18 Oct 2025</h2>
+            <h3>Holiday request</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
